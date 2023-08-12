21ad318e5cb9d5ce0b85d6b7d348a6421947f00c
Михалев Владислав Александрович <MikhalevVA1@hq.corp.mos.ru>
2023-08-12 15:27:31 +0300

    surrent state

--- hometask_2/auth_server_api.py
+++ hometask_2/auth_server_api.py
@@ -1,10 +1,14 @@
 from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.openapi.models import Response
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
 import hashlib
+
+from starlette.responses import HTMLResponse
+
 
 class AutorizeUserBody(BaseModel):
     pass
@@ -27,12 +31,41 @@ app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@app.post("/autorize_user")
-async def autorize_user(body: AutorizeUserBody):
-    pass #todo redirect user
 
-@app.post("/autentificate_user")
-async def autentificate(body: UserAutentificationForm):
+
+@app.get("/autorize_user", response_class=HTMLResponse)
+async def autorize_user():
+    return """
+    <!DOCTYPE html>
+    <html>
+       <body>
+          <form method="POST"  action="/login">
+             <label for="username">Username:</label><br>
+             <input type="text" id="username" name="username" value="johndoe@mail.com"><br>
+             <label for="password">Password:</label><br>
+             <input type="password" id="password" name="password" value="hunter2"><br><br>
+             <input type="submit" value="Submit">
+          </form>
+       </body>
+    </html>
+    """
+
+@app.post("/autorize_user")
+async def autentificate(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    email = form_data.username
+    password = form_data.password
+    print("login")
+    #    user = query_user(email)
+    #    if not user:
+    # you can return any response or error of your choice
+    #        raise InvalidCredentialsException
+    #    elif password != user['password']:
+    #        raise InvalidCredentialsException
+
+    #    token = manager.create_access_token(data={'sub': email})
+    #    response = RedirectResponse(url="/protected",status_code=status.HTTP_302_FOUND)
+    #    manager.set_cookie(response, token)
+    #    return response
     pass #todo login user
     #todo post_autorization_code() here if all right
 
@@ -59,7 +92,7 @@ async def client(body: ServiceBaseClass):
 
     # make a call to the model to authenticate
     createResponse = check_app(body.user_id, hashed_client_secret, autorization_code)
-    return {'success': createResponse}
+    return Response(status_code=status.HTTP_200_OK)
 
 
 @app.post("/signin")
--- hometask_2/task_tracker_server.py
+++ hometask_2/task_tracker_server.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI, HTTPException, status, Header
+from fastapi import Depends, FastAPI, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
@@ -8,16 +8,26 @@ from typing import Optional
 
 import hashlib
 import jwt
+from starlette.responses import Response
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+def verify_decor(method):
+    async def wrapper(req: Request):
+        if not verify(req.headers.get("access_token")):
+            return RedirectResponse(url="http://localhost:8000/login",  # todo вынести в параметры
+                                    status_code=status.HTTP_302_FOUND,
+                                    headers={"x-error": "Invalid credentials"})
+        await method(req)
+    return wrapper
+
 #todo add to safe component (database)
 AUTHSECRET = "BlaBlaBla"
 
 class TaskData(BaseModel):
-    pass
+    aaa: str
 
 def verify(token):
     try:
@@ -27,13 +37,11 @@ def verify(token):
         #print(error) #todo logs!
         return None#{"success": False}
 
-
 @app.post("/add_task", response_class=RedirectResponse)
-def add_task(body: TaskData, header: Optional[str] = Header(None)):
-    if not verify(header["access_token"]):
-        return RedirectResponse(header['redirect_url'],
-                                status_code=status.HTTP_302_FOUND,
-                                headers={"x-error": "Invalid credentials"}) #todo bad request
+@verify_decor
+async def add_task(req: Request):
+    print("aaaaa")
+
     #todo add task to database from task_tracker_base
 
 #todo others need to test
